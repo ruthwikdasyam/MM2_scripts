9b849fcb1dd43fb8be65c76b5e4fc254a54032c6
Ruthwik <ruthwik@umd.edu>
2025-04-09 14:25:22 -0400

    new log structure

--- memory.py
+++ memory.py
@@ -57,6 +57,9 @@ class MemoryNode:
     
     def response_reason_callback(self, data):
         self.response_reason = data.data
+        log = self.get_log(type="llm")
+        # print(json.dumps(log, indent=4))
+        self.save_logs(log)
     
     def task_status_callback(self, data):
         self.task_status = data.data
@@ -78,44 +81,51 @@ class MemoryNode:
         np_arr = np.frombuffer(msg.data, np.uint8)
         self.image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    def get_log(self):
+    def get_log(self, type):
+
         log = {}
-        # Timestamp
-        log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        # Robot Status and Position
-        log["robot"] = {
-            "status": {
-                "base_status": "Active" if self.subtask_name in self.loc_options else "Rest",
-                "arm_status": "Active" if self.subtask_name in self.arm_options else "Rest"
-            },
-            "position": {
-                "base_position": self.odom_entry,  #  list [x, y, z]
-                "arm_position": self.arm_pos
-            }}
-        # LLM Logs
-        log["llm"] = {
-            "user_query": self.user_query,
-            "response": self.response_sequence,
-            "reasoning": self.response_reason
-        }
-        # Camera Observation
-        # try:
-        if self.image is not None:
-            log["camera_observation"] = self.llm.get_vlm_feedback(task="caption", rs_image=self.image)
-        else:
-            log["camera_observation"] = " "
-        # except Exception as e:
-        #     log["camera_observation"] = f"Error: Could not capture image. ({str(e)})"
-        # Task Progress
-        log["task_progress"] = {
-            "task_name": self.subtask_name,
-            "task_status": self.task_status
-        }
-        # Return the log as a JSON string
-        return log
+        if type == "status":
+            # Timestamp
+            log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            # Robot Status and Position
+            log["robot"] = {
+                "status": {
+                    "base_status": "Active" if self.subtask_name in self.loc_options else "Rest",
+                    "arm_status": "Active" if self.subtask_name in self.arm_options else "Rest"
+                },
+                "position": {
+                    "base_position": self.odom_entry,  #  list [x, y, z]
+                    "arm_position": self.arm_pos
+                }}
+            # Camera Observation
+            # try:
+            if self.image is not None:
+                log["camera_observation"] = self.llm.get_vlm_feedback(task="caption", rs_image=self.image)
+            else:
+                log["camera_observation"] = " "
+            # except Exception as e:
+            #     log["camera_observation"] = f"Error: Could not capture image. ({str(e)})"
+            # Task Progress
+            log["task_progress"] = {
+                "task_name": self.subtask_name,
+                "task_status": self.task_status
+            }
+            # Return the log as a JSON string
+            return log
+        
+        elif type == "llm":
+            # Timestamp
+            log["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            # LLM Logs
+            log["llm"] = {
+                "user_query": self.user_query,
+                "response": self.response_sequence,
+                "reasoning": self.response_reason
+            }
+            return log
 
 
-    def save_logs(self, log_entry):
+    def save_logs(self, log):
         log_file = "memory_logs/robot_logs.jsonl"  # JSONL (JSON Lines) format for continuous logging
         try:
             json_line = json.dumps(log) 
@@ -133,7 +143,7 @@ if __name__ == '__main__':
 
         while True:
             print("logging...")
-            log = mem_node.get_log()
+            log = mem_node.get_log(type="status")
             print(json.dumps(log, indent=4))
             mem_node.save_logs(log)
             time.sleep(2)
